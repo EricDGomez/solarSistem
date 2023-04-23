d33065772dd9ecf6e301397a2727a8878984f898
Eric Gomez <ericdanielcc@icloud.com>
2023-04-23 15:48:08 -0400

    first 7 done

--- exercises/e8.js
+++ exercises/e8.js
@@ -5,10 +5,11 @@ import { data } from "../data/data";
 // Return example: 'Planet Name'
 
 export function findPlanetNameByMoon(data, moonName) {
-  // Your code goes here...
+  // Your code goes here...//ns
+  return data.planets
+    .find((planet) => planet.moons.includes(moonName))
+    .map((planet) => planet.name);
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
